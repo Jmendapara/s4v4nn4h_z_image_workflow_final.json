6fa9cdfe37b925ecc9bc5b351e9a04d5749e20cf
Jay Mendapara <jmendapara@gmail.com>
2026-02-23 15:21:17 -0500

    Update start.sh to include extra model paths for ComfyUI API serving, enhancing flexibility in model configuration.

--- src/start.sh
+++ src/start.sh
@@ -54,13 +54,15 @@ echo "worker-comfyui: Starting ComfyUI"
 : "${COMFY_LOG_LEVEL:=DEBUG}"
 
 # Serve the API and don't shutdown the container
+EXTRA_PATHS="--extra-model-paths-config /comfyui/extra_model_paths.yaml"
+
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen ${EXTRA_PATHS} --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python -u /comfyui/main.py --disable-auto-launch --disable-metadata ${EXTRA_PATHS} --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py
